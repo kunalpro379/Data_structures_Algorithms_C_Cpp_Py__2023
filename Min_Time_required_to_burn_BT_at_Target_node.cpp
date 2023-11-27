c7e97110f4daf3412482be5605a57ecf6e4cfc85
kunal deepak patil <132162026+kunalpro379@users.noreply.github.com>
2023-11-28 02:48:34 +0530

    Update Min_Time_required_to_burn_BT_at_Target_node.cpp

--- Min_Time_required_to_burn_BT_at_Target_node.cpp
+++ Min_Time_required_to_burn_BT_at_Target_node.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
+//User function Template for C++
 
-#include<string>
-using namespace std;
-#include<vector>
-#include<hash_map>
-#include<map>
-#include<queue>
+/*
 struct Node {
     int data;
     Node *left;
@@ -16,20 +11,93 @@ struct Node {
         left = right = NULL;
     }
 };
+*/
+//
+//1 parent ko point karnewala pointer available karaya  
+///node to parent mapping 
+//2 LOT
+//3 find target
+//4 BFS traversal 
+///Two data structures 
+//
 
+
+
+class Solution {
+public:
   //create mapping 
   //return target node
+    Node* CreateParentMapping(Node* root, int Target, map<Node*, Node*>& NodeToParent) {
+        Node* temp = NULL;
+        queue<Node*> Q;
+        Q.push(root);
+        
+        while (!Q.empty()) {
+            Node* front = Q.front();
+            Q.pop();
+            
+            if (front->data == Target) {
+                temp = front;
+            }
 
-// Driver Code
-int main() {
-    Solution solution;
-    string treeString = "1 2 3 N N 4 N N 5 N N";
- TreeNode* root = buildTree(treeString);
+            if (front->left) {
+                NodeToParent[front->left] = front;
+                Q.push(front->left);
+            }
 
-    int target = 3;
-    int result = solution.minTime(root, target);
+            if (front->right) {
+                NodeToParent[front->right] = front;
+                Q.push(front->right);
+            }
+        }
+        return temp;
+    }
 
-    cout << "Minimum time to burn the tree: " << result << endl;
+    int BurnTree(Node* root, map<Node*, Node*>& NodeToParent) {
+        map<Node*, bool> visited;
+        queue<Node*> Q;
+        Q.push(root);
+        visited[root] = true;
+        int TimeAns = 0;
 
-    return 0;
-}
+        while (!Q.empty()) {
+            bool help = false;
+            int Size = Q.size();
+
+            for (int i = 0; i < Size; i++) { //preocessiong Neighbouring Nodes
+                Node* front = Q.front();
+                Q.pop();
+
+                if (front->left && !visited[front->left]) {
+                    help = true;
+                    Q.push(front->left);
+                    visited[front->left] = true;
+                }
+
+                if (front->right && !visited[front->right]) {
+                    help = true;
+                    Q.push(front->right);
+                    visited[front->right] = true;
+                }
+
+                if (NodeToParent[front] && !visited[NodeToParent[front]]) {
+                    help = true;
+                    Q.push(NodeToParent[front]);
+                    visited[NodeToParent[front]] = true;
+                }
+            }
+
+            if (help) {
+                TimeAns++;
+            }
+        }
+        return TimeAns;
+    }
+
+    int minTime(Node* root, int target) {
+        map<Node*, Node*> NodeToParent;
+        Node* TargetNode = CreateParentMapping(root, target, NodeToParent);
+        int TimeAns = BurnTree(TargetNode, NodeToParent);
+        return TimeAns;
+    }
+};

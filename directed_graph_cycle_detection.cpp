5f2293db645e9c2969b8c108549b61b49744f437
kunal deepak patil <132162026+kunalpro379@users.noreply.github.com>
2023-11-30 01:53:18 +0530

    Update directed_graph_cycle_detection.cpp

--- directed_graph_cycle_detection.cpp
+++ directed_graph_cycle_detection.cpp
@@ -1,6 +1,9 @@
+///using kahns algorithem   
 #include <list>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -43,3 +46,58 @@ int detectCycleInDirectedGraph(int n, vector<pair<int, int>>& edges) {
     }
     return false;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///                   USING K A H N S  ALGORITHEMS
+///
+////////////////////////////////////////////////////////////////////////////////////////
+int detectCycleInDirectedGraph(int n, vector<pair<int, int>>& edges) {
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < edges.size(); i++) {
+        int u = edges[i].first - 1;
+        int v = edges[i].second - 1;
+        adj[u].push_back(v);
+    }
+
+    // Find all in-degrees
+    vector<int> Indegree(n);
+    for (auto i : adj) {
+        for (auto j : i.second) {
+            Indegree[j]++;
+        }
+    }
+
+    // 0 in-degree nodes push into the queue
+    queue<int> Q;
+    for (int i = 0; i < n; i++) {
+        if (Indegree[i] == 0) {
+            Q.push(i);
+        }
+    }
+
+    // Perform BFS
+    int cnt = 0;
+    while (!Q.empty()) {
+        int front = Q.front();
+        Q.pop();
+        // ans store
+        cnt++;
+
+        // neighbor's in-degree check and update
+        for (auto neighbour : adj[front]) {
+            Indegree[neighbour]--;
+            if (Indegree[neighbour] == 0) {
+                Q.push(neighbour);
+            }
+        }
+    }
+
+    // If the count of processed nodes is equal to the number of nodes, there is no cycle
+    if (cnt == n) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
